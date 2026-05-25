4be7e705af79e4cdee2766444897e1b0b2a59f5b
JonathanLuna-code <202000688c@utea.edu.pe>
2026-05-25 12:04:36 -0500

    configuracion de migracion

--- database/migrations/2026_05_25_165655_create_events_table.php
+++ database/migrations/2026_05_25_165655_create_events_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('featured');
+            $table->timestamp();
+            $table->time();
+            $table->string('location');
             $table->timestamps();
         });
     }
