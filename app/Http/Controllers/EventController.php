74d3516bbcba3849525593705eb6dece3ec2ad13
JonathanLuna-code <202000688c@utea.edu.pe>
2026-05-25 20:47:44 -0500

    Implementacion de codigo en ReadEventTest.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Http\Requests\StoreEventRequets;
+use Illuminate\Http\RedirectResponse;
 
 class EventController extends Controller
 {
-    public function store(StoreEventRequets $request)
+    public function store(StoreEventRequets $request): RedirectResponse
     {
         $eventData = $request->all();
 
---
+++ tests/Feature/ReadEventTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Event;
+use carbon\Carbon;
+
+class ReadEventTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_can_display_list_of_events(): void
+    {
+        // Arrange: 
+        Event::create([
+            'name' => 'Evento 1',
+            'featured' => 'logo.png',
+            'date' => Carbon::now(),
+            'time' => '12:00:00',
+            'location' => 'Aula 504 bloque L de la UTEA',
+        ]);
+
+        Event::create([
+            'name' => 'Evento 2',
+            'featured' => 'logo.png',
+            'date' => Carbon::now(),
+            'time' => '12:00:00',
+            'location' => 'Aula 504 bloque L de la UTEA',
+        ]);
+
+        // Act:
+        $response = $this->get('/events');
+
+        // Assert:
+        $response->assertStatus(200);
+        
+        $response->assertSee('Evento 1');
+        $response->assertSee('Evento 2');
+    }
+}
