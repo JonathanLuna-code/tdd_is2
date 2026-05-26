93a413a5adebb2b7987b30f6e9e8418e31df818d
JonathanLuna-code <202000688c@utea.edu.pe>
2026-05-25 20:26:22 -0500

    correcciones de codigo

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,7 +10,13 @@ class EventController extends Controller
     public function store(Request $request)
     {
         $eventData = $request->all();
-
+        /**
+         * array:5 [// app/Http/Controllers/EventController.php:11
+             * "name" => "Clase de IS2"
+             * "featured" => "logo.png"
+         * ]
+         */
+        
         Event::create($eventData);
 
         return redirect()->route('events.index');
