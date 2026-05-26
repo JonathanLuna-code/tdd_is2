360d13e16028397c4bb649d5e19276b59ce943db
JonathanLuna-code <202000688c@utea.edu.pe>
2026-05-25 19:25:51 -0500

    eliiminacion de exampletest.php y ajuste e implementacion de codigo

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        return redirect()->route('events.index');
+    }
 }
