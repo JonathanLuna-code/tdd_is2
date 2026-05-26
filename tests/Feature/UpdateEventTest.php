1e52b0679ea9e17837c21e15c9d951fa3ae3fe50
JonathanLuna-code <202000688c@utea.edu.pe>
2026-05-25 21:16:10 -0500

    Se agrego UpdateEventTest con funcionalidad de configuración y actualización.

--- tests/Feature/UpdateEventTest.php
+++ tests/Feature/UpdateEventTest.php
@@ -5,16 +5,44 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Event;
+use carbon\Carbon;
 
 class UpdateEventTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      */
+
+    protected $event;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        //Crear event:
+        $this->event = Event::create([
+            'name' => 'Evento a ser actualizado',
+            'featured' => 'evento.png',
+            'date' => Carbon::now(),
+            'time' => '21:00:00',
+            'location' => 'Ubicación sin actualizar',
+        ]);
+    }
+
     public function test_example(): void
     {
-        $response = $this->get('/');
+        // Arrange:
+        $updatedData = [
+            'name' => 'Evento actualizado',
+        ];
 
+        // Act:
+        $response = $this->put('/events/' . $this->event->id, $updatedData);
+
+        // Assert:
         $response->assertStatus(200);
+        $this->assertDatabaseHas('events', $updatedData);
     }
 }
